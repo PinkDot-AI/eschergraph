aa169871464a4f262d2b24cc56d23a6c2f7d083b
BjarneBepaData <bjarne@bepadata.nl>
2024-06-26 17:14:09 +0200

    Adding some dependencies and pre-commit hooks to the project

---
+++ pinkgraph/main.py
@@ -0,0 +1,10 @@
+# This is an example file with the Python function docstring
+from __future__ import annotations
+
+
+def main() -> None:
+    """
+    The (initial) main function.
+    With an example docstring.
+    """
+    print("Welcome to the pinkgraph library!")
