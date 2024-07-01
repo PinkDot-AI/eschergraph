405a21d141270dde9060f758072ef7e29cb45ce6
BjarneBepaData <bjarne@bepadata.nl>
2024-07-01 12:33:28 +0200

    Making some changes to the ruff config and the docstring convention used

--- pinkgraph/main.py
+++ pinkgraph/main.py
@@ -1,10 +1,9 @@
-# This is an example file with the Python function docstring
 from __future__ import annotations
 
 
 def main() -> None:
-    """
-    The (initial) main function.
-    With an example docstring.
-    """
-    print("Welcome to the pinkgraph library!")
+  """The (initial) main function.
+
+  With an example docstring.
+  """
+  print("Welcome to the pinkgraph library!")
