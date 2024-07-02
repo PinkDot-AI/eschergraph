f9140a2736953f851d012bdbda226e71ef43b2f6
BjarneBepaData <bjarne@bepadata.nl>
2024-07-02 10:20:23 +0200

    Add custom exception and check to ensure that the promps are completely formatted

--- pinkgraph/agents/jinja_helper.py
+++ pinkgraph/agents/jinja_helper.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import re
+from typing import Any
+
 from jinja2 import Environment
 from jinja2 import FileSystemLoader
 from jinja2 import select_autoescape
@@ -24,3 +27,16 @@ def process_template(template_file: str, data: dict[str, str]) -> str:
   )
   template: Template = jinja_env.get_template(template_file)
   return template.render(**data)
+
+
+def extract_variables(template_string: str) -> list[Any]:
+  """Extract all variables in a Jinja template in string format.
+
+  Args:
+    template_string (str): the jinja template as a string.
+
+  Returns:
+    A list of all the identified variables in the string template.
+  """
+  variable_pattern: str = r"\{\{ *([\w_]+) *\}\}"
+  return re.findall(variable_pattern, template_string)
--- pinkgraph/exceptions.py
+++ pinkgraph/exceptions.py
@@ -3,3 +3,10 @@ from __future__ import annotations
 
 class BasePinkgraphException(Exception):
   """The base class for all Pinkgraph exceptions."""
+
+
+class PromptFormattingException(BasePinkgraphException):
+  """When some jinja prompt variables have not been formatted.
+
+  Used to check if the prompt has been sent to the LLM / agent as intended.
+  """
