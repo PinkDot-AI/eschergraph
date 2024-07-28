71b5f0ba752eabb02edc9c2701a0cace8134a08e
BjarneBepaData <bjarne@bepadata.nl>
2024-07-28 17:00:59 +0200

    Creating a class hierarchy with a base class for persistence

--- eschergraph/graph/metadata.py
+++ eschergraph/graph/metadata.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from uuid import UUID
+
 from attrs import define
 
 
@@ -7,5 +9,9 @@ from attrs import define
 class Metadata:
   """The metadata that is attached to a part of the graph."""
 
-  document_id: int
+  document_id: UUID
   chunk_id: int
+
+  def __hash__(self) -> int:
+    """The hash method that hashes the id and the chunk and sums them."""
+    return hash(self.document_id) + hash(self.chunk_id)
--- eschergraph/graph/node.py
+++ eschergraph/graph/node.py
@@ -1,27 +1,29 @@
 from __future__ import annotations
 
+from typing import Optional
+
 from attrs import define
 from attrs import field
 
 from eschergraph.graph.metadata import Metadata
-from eschergraph.storage import persistence
+from eschergraph.graph.persistence import EscherBase
 
 
-@persistence
 @define
-class Node:
+class Node(EscherBase):
   """A node in the graph.
 
   A node can be an entity from the source text, or a community formed
   from a (sub)graph of extracted entities.
   """
 
-  name: str
-  description: str
-  metadata: Metadata
-  level: int
+  name: str = field(kw_only=True)
+  description: str = field(kw_only=True)
+  level: int = field(kw_only=True)
   """The level at which the node occurs. Level 0 refers to directly extracted entities, and levels
   above that are aggregated communities."""
+  community: Optional[Node] = None
+  metadata: set[Metadata] = field(factory=set)
   properties: list[str] = field(factory=list)
   child_nodes: list[Node] = field(factory=list)
   report: list[dict[str, str]] = field(factory=list)
