5df3ce12460c8011a6a9e4e91b5fa976ba8fd0a0
BjarneBepaData <bjarne@bepadata.nl>
2024-07-30 14:11:45 +0200

    A start with the repository pattern and the new node structure

--- eschergraph/exceptions.py
+++ eschergraph/exceptions.py
@@ -1,16 +1,20 @@
 from __future__ import annotations
 
 
-class BasePinkgraphException(Exception):
+class BaseEscherGraphException(Exception):
   """The base class for all Pinkgraph exceptions."""
 
 
-class PromptFormattingException(BasePinkgraphException):
+class PromptFormattingException(BaseEscherGraphException):
   """When some jinja prompt variables have not been formatted.
 
   Used to check if the prompt has been sent to the LLM / agent as intended.
   """
 
 
-class ExternalProviderException(BasePinkgraphException):
+class ExternalProviderException(BaseEscherGraphException):
   """When something unexpected occurs during an interation with an external service."""
+
+
+class DataLoadingException(BaseEscherGraphException):
+  """Raised when some data on the EscherGraph objects has not been loaded as expected."""
--- eschergraph/graph/node.py
+++ eschergraph/graph/node.py
@@ -5,15 +5,21 @@ from typing import TYPE_CHECKING
 
 from attrs import define
 from attrs import field
+from attrs import fields_dict
 
+from eschergraph.exceptions import DataLoadingException
 from eschergraph.graph.base import EscherBase
+from eschergraph.graph.base import LoadState
+from eschergraph.graph.persistence import NodeRepository
 
 # To prevent circular import errors
 if TYPE_CHECKING:
   from eschergraph.graph.edge import Edge
 
+# TODO: add a factory method to return the default NodeRepository
 
-@define(hash=True)
+
+@define
 class Node(EscherBase):
   """A node in the graph.
 
@@ -21,13 +27,191 @@ class Node(EscherBase):
   from a (sub)graph of extracted entities.
   """
 
-  name: str = field(kw_only=True)
-  description: str = field(kw_only=True)
-  level: int = field(kw_only=True)
+  _name: Optional[str] = field(default=None, metadata={"group": LoadState.CORE})
+  _description: Optional[str] = field(default=None, metadata={"group": LoadState.CORE})
+  _level: Optional[int] = field(default=None, metadata={"group": LoadState.CORE})
   """The level at which the node occurs. Level 0 refers to directly extracted entities, and levels
   above that are aggregated communities."""
-  community: Optional[Node] = None
-  properties: list[str] = field(factory=list, hash=False)
-  child_nodes: list[Node] = field(factory=list, hash=False)
-  edges: set[Edge] = field(factory=set, hash=False)
-  report: list[dict[str, str]] = field(factory=list, hash=False)
+  _properties: Optional[list[str]] = field(
+    default=None, metadata={"group": LoadState.CORE}
+  )
+  _edges: Optional[set[Edge]] = field(
+    default=None, metadata={"group": LoadState.CONNECTED}
+  )
+  _community: Optional[Node] = field(default=None, metadata={"group": LoadState.FULL})
+  _child_nodes: Optional[list[Node]] = field(
+    default=None, metadata={"group": LoadState.FULL}
+  )
+  _report: Optional[list[dict[str, str]]] = field(
+    default=None, metadata={"group": LoadState.FULL}
+  )
+  loadstate: LoadState = field(default=LoadState.REFERENCE)
+  """The attribute that keeps track of the loading state of a Node."""
+  repository: NodeRepository = field(kw_only=True)
+
+  @property
+  def name(self) -> str:
+    """The getter for the node name.
+
+    Returns:
+      The node's name.
+    """
+    self._check_loadstate(attr_name="_name")
+
+    if not self._name:
+      raise DataLoadingException("Node name has not been loaded.")
+
+    return self._name
+
+  @property
+  def description(self) -> str:
+    """The getter for the node description.
+
+    Returns:
+      The node's description.
+    """
+    self._check_loadstate(attr_name="_description")
+
+    if not self._description:
+      raise DataLoadingException("Node description has not been loaded.")
+
+    return self._description
+
+  @property
+  def level(self) -> int:
+    """The getter for the node level.
+
+    The level indicates how close the node is to extraction from the source text.
+    Level 0 indicates that the node has been extracted from the source text.
+    Level 1 would correspond to the direct community of this node and so on.
+
+    Returns:
+      The node's level.
+    """
+    self._check_loadstate(attr_name="_level")
+
+    if not self._level:
+      raise DataLoadingException("Node level has not been loaded.")
+
+    return self._level
+
+  @property
+  def properties(self) -> list[str]:
+    """The getter for the node properties.
+
+    Returns:
+      The node's properties.
+    """
+    self._check_loadstate(attr_name="_properties")
+
+    if not isinstance(self._properties, list):
+      raise DataLoadingException("Node properties have not been loaded.")
+
+    return self._properties
+
+  @property
+  def edges(self) -> set[Edge]:
+    """The getter for the node edges.
+
+    Returns:
+      The node's edges.
+    """
+    self._check_loadstate(attr_name="edges")
+
+    if not isinstance(self._edges, set):
+      raise DataLoadingException("Node edges have not been loaded.")
+
+    return self._edges
+
+  @property
+  def community(self) -> Optional[Node]:
+    """The getter for the node community.
+
+    Returns:
+      The node's community or None if the node is not in a community.
+    """
+    self._check_loadstate(attr_name="_community")
+
+    # Different check as node cannot have a community (top-layer)
+    if not self.loadstate == fields_dict(Node)["_community"].metadata["group"]:
+      raise DataLoadingException("The node community has not been loaded.")
+
+    return self._community
+
+  @property
+  def child_nodes(self) -> list[Node]:
+    """The getter for the child nodes.
+
+    Returns:
+      The node's children.
+    """
+    self._check_loadstate(attr_name="_child_nodes")
+
+    if not isinstance(self._child_nodes, list):
+      raise DataLoadingException("The child nodes have not been loaded.")
+
+    return self._child_nodes
+
+  @property
+  def report(self) -> list[dict[str, str]]:
+    """The getter for the node report.
+
+    Returns:
+      The node's report.
+    """
+    self._check_loadstate(attr_name="_report")
+
+    if not isinstance(self._report, list):
+      raise DataLoadingException("The node report has not been loaded.")
+
+    return self._report
+
+  def _check_loadstate(self, attr_name: str) -> None:
+    """Check if the attribute has been loaded by the current loadstate.
+
+    If not enough has been loaded, then load more instance data from the repository.
+
+    Args:
+      attr_name (str): The name of the attribute that starts with an underscore.
+    """
+    required_loadstate: LoadState = fields_dict(Node)[attr_name].metadata["group"]
+
+    # Load more instance data from the repository if load state is too small
+    if self.loadstate.value < required_loadstate.value:
+      self.repository.load(self, loadstate=required_loadstate)
+
+  @classmethod
+  def create_node(
+    cls,
+    name: str,
+    description: str,
+    level: int,
+    repository: NodeRepository,
+    properties: Optional[list[str]] = None,
+  ) -> Node:
+    """The method that allows for the creation of a new node.
+
+    When created, the load state is considered to be full and all attributes
+    are set to their defaults.
+
+    Args:
+      name (str): The name of the node.
+      description (str): The node description.
+      level (int): The level of the node.
+      repository (NodeRepository): The repository that will store the node.
+      properties (Optional[list[str]]): The optional properties for the node.
+
+    Returns:
+      The node that has been created.
+    """
+    return cls(
+      name=name,
+      description=description,
+      level=level,
+      properties=properties if properties else [],
+      repository=repository,
+      edges=set(),
+      child_nodes=[],
+      report=[],
+      loadstate=LoadState.FULL,
+    )
