daa87a8159681ebde3e424d5e3c433a29b53ccc3
BjarneBepaData <bjarne@bepadata.nl>
2024-07-30 17:36:10 +0200

    Last commit of the day

---
+++ eschergraph/graph/graph.py
@@ -0,0 +1,5 @@
+# from attrs import define
+# from eschergraph.graph.node import Node
+# from eschergraph.graph.edge import Edge
+# TODO: add a factory method to return the default Repository
+from __future__ import annotations
