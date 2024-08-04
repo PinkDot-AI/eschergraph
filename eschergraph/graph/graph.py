eae7f032652951d6687b5fcc9713600a6f51f92d
BjarneBepaData <bjarne@bepadata.nl>
2024-08-04 16:04:09 +0200

    Test the basic getter and setters that are added with the class decorator

--- eschergraph/graph/graph.py
+++ eschergraph/graph/graph.py
@@ -1,17 +1,18 @@
 from __future__ import annotations
 
 from attrs import define
+from attrs import field
 
 from eschergraph.graph.persistence import Repository
+from eschergraph.graph.persistence.factory import get_default_repository
 
 
 @define
 class Graph:
   """The EscherGraph graph class."""
 
-  repository: Repository
   name: str
-  save_location: str
+  repository: Repository = field(factory=get_default_repository)
 
   # TODO: a method to add a node
   # TODO: a method to add an edge
