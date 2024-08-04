ed516ae8d904e28f5905a7843646a71af74dd722
BjarneBepaData <bjarne@bepadata.nl>
2024-08-04 11:13:51 +0200

    A bit of refactoring of the tests and adding a test for when the loadstate decreases

--- tests/graph/test_utils.py
+++ tests/graph/test_utils.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from unittest.mock import Mock
 from uuid import uuid4
 
 import pytest
@@ -8,7 +9,6 @@ from eschergraph.graph.base import EscherBase
 from eschergraph.graph.community import Community
 from eschergraph.graph.loading import LoadState
 from eschergraph.graph.persistence import Metadata
-from eschergraph.graph.persistence import Repository
 from eschergraph.graph.utils import _extract_inner_type
 from eschergraph.graph.utils import _extract_property_type
 from eschergraph.graph.utils import _parse_future_annotations
@@ -16,12 +16,12 @@ from eschergraph.graph.utils import loading_getter_setter
 
 
 @pytest.fixture(scope="function")
-def base_repository(mock_repository: Repository) -> Repository:
+def base_repository(mock_repository: Mock) -> Mock:
   # Set the metadata equal to an empty set
   def load_side_effect(base: EscherBase, loadstate: LoadState) -> None:
     base._metadata = set()
 
-  mock_repository.load.side_effect = load_side_effect  # type: ignore
+  mock_repository.load.side_effect = load_side_effect
 
   return mock_repository
 
@@ -57,14 +57,14 @@ def test_parse_future_annotations() -> None:
 class ExtendedBase(EscherBase): ...
 
 
-def test_check_loadstate_metadata(base_repository: Repository) -> None:
+def test_check_loadstate_metadata(base_repository: Mock) -> None:
   base: EscherBase = ExtendedBase(repository=base_repository)
 
   assert isinstance(base.metadata, set)
   assert base.loadstate == LoadState.CORE
 
 
-def test_setting_metadata(base_repository: Repository) -> None:
+def test_setting_metadata(base_repository: Mock) -> None:
   base: EscherBase = ExtendedBase(repository=base_repository)
 
   metadata_set: set[Metadata] = {Metadata(document_id=uuid4(), chunk_id=1)}
