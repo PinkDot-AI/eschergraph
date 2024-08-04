ec90deceee93abc482c0a4042fe34e4e06e50206
BjarneBepaData <bjarne@bepadata.nl>
2024-08-04 10:58:07 +0200

    Adding some more tests for the utils

--- tests/graph/test_utils.py
+++ tests/graph/test_utils.py
@@ -1,11 +1,17 @@
 from __future__ import annotations
 
+from uuid import uuid4
+
 import pytest
 
 from eschergraph.graph.base import EscherBase
+from eschergraph.graph.community import Community
 from eschergraph.graph.loading import LoadState
+from eschergraph.graph.persistence import Metadata
 from eschergraph.graph.persistence import Repository
+from eschergraph.graph.utils import _extract_inner_type
 from eschergraph.graph.utils import _extract_property_type
+from eschergraph.graph.utils import _parse_future_annotations
 from eschergraph.graph.utils import loading_getter_setter
 
 
@@ -27,6 +33,23 @@ def test_extract_property_type_string() -> None:
   assert _extract_property_type("") == ""
 
 
+def test_extract_inner_type() -> None:
+  assert _extract_inner_type("") == ""
+  assert _extract_inner_type("list[str]") == ""
+  assert _extract_inner_type("Optional[set[int]]") == "set"
+  assert _extract_inner_type("Optional[Node]") == "Node"
+
+
+def test_parse_future_annotations() -> None:
+  with pytest.raises(RuntimeError):
+    _parse_future_annotations("")
+    _parse_future_annotations("list[str]")
+
+  _parse_future_annotations("Optional[set[int]]") == set
+  _parse_future_annotations("Optional[list[str]]") == list
+  _parse_future_annotations("Optional[Community]") == Community
+
+
 # Testing whether the class decorator works.
 # Note that it cannot be applied to the base class directly as that would
 # trigger a circular import.
@@ -39,3 +62,16 @@ def test_check_loadstate_metadata(base_repository: Repository) -> None:
 
   assert isinstance(base.metadata, set)
   assert base.loadstate == LoadState.CORE
+
+
+def test_setting_metadata(base_repository: Repository) -> None:
+  base: EscherBase = ExtendedBase(repository=base_repository)
+
+  metadata_set: set[Metadata] = {Metadata(document_id=uuid4(), chunk_id=1)}
+  assert not base._metadata
+  assert base.loadstate == LoadState.REFERENCE
+
+  base.metadata = metadata_set
+
+  assert base.metadata == metadata_set
+  assert base.loadstate == LoadState.CORE  # type: ignore
