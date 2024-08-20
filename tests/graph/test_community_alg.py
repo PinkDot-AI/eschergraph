a552cf5e8ac7461d50e3990226ab19d79c258693
BjarneBepaData <bjarne@bepadata.nl>
2024-08-20 16:42:57 +0200

    The leidenalg community function has been added with tests

--- tests/graph/test_community_alg.py
+++ tests/graph/test_community_alg.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
+from uuid import UUID
+
 import igraph as ig
 
 from eschergraph.graph.community_alg import _create_level_igraph
+from eschergraph.graph.community_alg import get_leidenalg_communities
 from tests.graph.help import create_simple_extracted_graph
 
 
@@ -13,3 +16,11 @@ def test_create_level_igraph() -> None:
 
   assert {v["name"] for v in igraph.vs} == {node.id for node in nodes}
   assert len(list(igraph.es)) == len(edges)
+
+
+def test_get_leidenalg_communities() -> None:
+  _, nodes, _ = create_simple_extracted_graph()
+  partitions: list[list[UUID]] = get_leidenalg_communities(nodes)
+
+  assert {n.id for n in nodes} == {id for p in partitions for id in p}
+  assert len(partitions) < len(nodes)
