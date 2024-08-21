4e64bf22672127440050290328575c3edda84a41
BjarneBepaData <bjarne@bepadata.nl>
2024-08-21 15:06:39 +0200

    Rewrote the visualization and removed a dependency

--- tests/visualization/conftest.py
+++ tests/visualization/conftest.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
 import random
+from uuid import UUID
 
 import pytest
 from faker import Faker
+
+from eschergraph.graph import Edge
+from eschergraph.graph import Node
+from eschergraph.graph.community_alg import get_leidenalg_communities
+from tests.graph.help import create_simple_extracted_graph
 
 faker: Faker = Faker()
 
@@ -17,3 +23,22 @@ def node_name_comms() -> list[list[str]]:
     name_comms.append([faker.name() for _ in range(num_nodes)])
 
   return name_comms
+
+
+@pytest.fixture(scope="function")
+def community_graph() -> tuple[list[list[Node]], list[Edge]]:
+  _, nodes, edges = create_simple_extracted_graph()
+  node_ids: list[list[UUID]] = get_leidenalg_communities(nodes).partitions
+
+  node_dict: dict[UUID, Node] = {node.id: node for node in nodes}
+
+  # Transform the list of node_ids into a list of nodes
+  comms: list[list[Node]] = []
+  for comm in node_ids:
+    comm_nodes: list[Node] = []
+    for id in comm:
+      comm_nodes.append(node_dict[id])
+
+    comms.append(comm_nodes)
+
+  return comms, edges
