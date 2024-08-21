413739dba64257c8e16b4c57245c4332f6b19585
BjarneBepaData <bjarne@bepadata.nl>
2024-08-21 11:14:22 +0200

    Some changes to the communities, and bumped the required python version for networkx

--- eschergraph/visualization/community.py
+++ eschergraph/visualization/community.py
@@ -2,22 +2,30 @@ from __future__ import annotations
 
 import seaborn as sns
 
+from eschergraph.graph import Edge
 from eschergraph.graph import Node
 
 
-def visualize_community(comms: list[list[Node]]) -> None:
+def visualize_community(comms: list[list[Node]], edges: list[Edge]) -> None:
   """Visualize a graph of communities.
 
-  Given a list containing lists of nodes, where each
+  Communities are provided in a list containing lists of nodes, where each
   list of nodes corresponds to a community.
 
   Args:
     comms (list[list[node]]): A list of communities.
+    edges (list[Edge]): The list of edges in the community graph.
   """
   ...
 
 
-def _community_to_colors(comms: list[list[str]]) -> None:
+def _community_to_colors(comms: list[list[str]]) -> list[dict[str, str | int]]:
   palette: list[str] = sns.color_palette("hls", len(comms)).as_hex()
-
-  return
+  colored_nodes: list[dict[str, str | int]] = []
+  group: int = 0
+  for comm in comms:
+    color: str = palette.pop()
+    group += 1
+    for node in comm:
+      colored_nodes.append({"node": node, "color": color, "group": group})
+  return colored_nodes
