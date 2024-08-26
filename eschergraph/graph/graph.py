24dea105c77710184d0ce07db1e42865e2458a5a
Lennart Bob <lennarttimmermans@gmail.com>
2024-08-26 10:37:38 +0200

    finisched sync logic and added test

--- eschergraph/graph/graph.py
+++ eschergraph/graph/graph.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
-from typing import Any
-from uuid import UUID
+import os
 
 from attrs import define
 from attrs import field
+from dotenv import load_dotenv
 
+from eschergraph.agents.embedding import Embedding
+from eschergraph.agents.providers.openai import ChatGPT
+from eschergraph.agents.providers.openai import OpenAIModel
 from eschergraph.graph.edge import Edge
 from eschergraph.graph.node import Node
 from eschergraph.graph.persistence import Metadata
@@ -13,7 +16,9 @@ from eschergraph.graph.persistence import Repository
 from eschergraph.graph.persistence.factory import get_default_repository
 from eschergraph.graph.persistence.vector_db import get_vector_db
 from eschergraph.graph.persistence.vector_db import VectorDB
-from eschergraph.graph.property import Property
+from eschergraph.tools.prepare_sync_data import prepare_sync_data
+
+load_dotenv()
 
 
 @define
@@ -23,11 +28,23 @@ class Graph:
   name: str
   repository: Repository = field(factory=get_default_repository)
   vector_db: VectorDB = field(factory=get_vector_db)
-  # embedding_model:Embedding =
+  embedding_model: Embedding | None = field(factory=None)
 
   def __init__(self) -> None:
     """This is the initializer of the graph class for getting the vectordb."""
     self.vector_db = get_vector_db()
+    # Retrieve the API key from the environment
+    api_key = os.getenv("OPENAI_API_KEY")
+    if api_key is None:
+      raise ValueError(
+        "API key is not set. Please set the OPENAI_API_KEY environment variable."
+      )
+
+    # Initialize the embedding model if it's not already provided
+    if self.embedding_model is None:
+      self.embedding_model = ChatGPT(
+        model=OpenAIModel.TEXT_EMBEDDING_LARGE, api_key=api_key
+      )
 
   def add_node(
     self,
@@ -96,53 +113,26 @@ class Graph:
         collection_name (str): The name of the vector database collection where documents should be stored.
         level (int, optional): The hierarchical level at which the metadata is being synced. Default is 0.
     """
-    docs: list[str] = []
-    ids: list[UUID] = []
-    metadata = list[dict[str, Any]]
-    for log in self.repository.get_change_log():
-      if isinstance(log.type, Node):
-        if log.action == "delete" or log.action == "update":
-          self.vector_db.delete_with_id([log.id], collection_name)
-          if log.action == "delete":
-            continue
-        node: Node = self.repository.get_node_by_id(log.id)
-        docs.append(node.name)
-        metadata.append({
-          "level": level,
-          "type": "node",
-          "entity_frm": "",
-          "entity_to": "",
-          "chunk_id": "",  # node.metadata.chunk_id, #node is currently a set of metadata but chroma cannot handle that...
-          "document_id": "",  # node.metadata.document_id
-        })
-      elif isinstance(log.type, Edge):
-        if log.action == "delete" or log.action == "update":
-          self.vector_db.delete_with_id([log.id], collection_name)
-          if log.action == "delete":
-            continue
-        edge: Edge = self.repository.get_edge_by_id(log.id)
-        docs.append(edge.description)
-        metadata.append({
-          "level": level,
-          "type": "edge",
-          "entity_frm": edge.frm.name,
-          "entity_to": edge.to.name,
-          "chunk_id": "",  # edge.metadata.chunk_id,
-          "document_id": "",  # edge.metadata.document_id
-        })
-      elif isinstance(log.type, Edge):
-        if log.action == "delete" or log.action == "update":
-          self.vector_db.delete_with_id([log.id], collection_name)
-          if log.action == "delete":
-            continue
-        property: Property = self.repository.get_property_by_id(log.id)
-        docs.append(property.description)
-        metadata.append({
-          "level": level,
-          "type": "property",
-          "entity_frm": property.node.name,
-          "entity_to": "",
-          "chunk_id": "",  # edge.metadata.chunk_id,
-          "document_id": "",  # edge.metadata.document_id
-        })
-      ids.append(log.id)
+    # Prepare data for synchronization
+    docs, ids, metadata, ids_to_delete = prepare_sync_data(
+      repository=self.repository, level=level
+    )
+
+    # Delete all records marked for deletion
+    if ids_to_delete:
+      self.vector_db.delete_with_id(ids_to_delete, collection_name)
+
+    # Embed all new or updated entries and insert into the vector database
+    if docs:
+      if self.embedding_model:
+        embeddings: list[list[float]] = self.embedding_model.get_embedding(
+          list_text=docs
+        )
+
+        self.vector_db.insert_documents(
+          embeddings=embeddings,
+          documents=docs,
+          ids=ids,
+          metadata=metadata,
+          collection_name=collection_name,
+        )
